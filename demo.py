5dae1819b9a65f0d4337ec1a6cfe42bcafc6fa7b
屮0v0屮 <ZAWALUDO@126.com>
2023-05-14 13:32:37 +0800

    程序名称变更：demo变更为MainProgram ; demo2变更为demo

--- demo.py
+++ demo.py
@@ -1,43 +1,36 @@
-import os
-import traceback
-
-import execjs
 import requests
-
+import execjs
 cookies = {
-    'WM_TID': '%2BXq3UQoVXFDTbYS%2Bz7FIfJJ2ba5VB6Us',
-    '_ntes_nuid': 'aa976aeaebcb6ce1d6e06841c28b8361',
-    'NTES_P_UTID': 'Xj56DglO4lw23wOVQrEWBLMZ4Yt3JBe8|1665506437',
-    'P_INFO': 'zawaludo@126.com|1667570667|0|mail126|00&99|null&null&null#gud&440100#10#0#0|&0||zawaludo@126.com',
-    'FPTOKEN': 'eVnFqX8NCtN21+OTxj7CdiXnH70FkZ834Wqi6qUibQNbkq+24HEDux4TMUA+EQY538RiPmBLYSY7z0FWrgYDXtgSVDBC/8bprhkoKQ/meXGcRaggmiTwRxiL3FpGT1Qi8HQyF+9iNDoBZM9qgyxTqBxNxfSF+QEhbmmO5G4g51aqRWFcWcgsXQAKYUgdR638slJ36MXDV7mJLpK4bvXs/vwATd34P02Hl+Ecw/uEkYz2JE+UFi09y1nWMnWM6H/L+2w1F7RYjfnz/N5HrN5xWJKrHsf0DWhoErhvlyH8PSLfQE2dE8092G2aldfVjxLzGwrlPQz89oSOJkfgwouIfYyzfiZszb8xEu9pvsJmI19HxCu5DY52few5338XG6O6DbLPK1EbUXE3/dvaZa624w==|H+plIZV8jJyp6lcu2b312MdWPGRygExW6MqMgS8H7bU=|10|e58cf226c60abdf4cd6d1c05d9b16922',
-    '_ntes_nnid': 'aa976aeaebcb6ce1d6e06841c28b8361,1676775425704',
-    'NMTID': '00OEmGeeJc32OwIXEzZufM_xp_EDJwAAAGGZ5qG6g',
+    '__bid_n': '187385d55219a699254207',
+    'P_INFO': 'zawaludo@126.com|1680361713|0|mail126|00&99|null&null&null#gud&440100#10#0#0|&0|mail126|zawaludo@126.com',
+    '_ntes_nnid': 'e320573ab9dcf82fee6ce0bd1fde46ec,1680829462419',
+    '_ntes_nuid': 'e320573ab9dcf82fee6ce0bd1fde46ec',
+    'NMTID': '00OUohNvG-YwixOHkjphD1aA8XEaIMAAAGHWT43ng',
     'WEVNSM': '1.0.0',
-    'WNMCID': 'uhpsor.1676775426459.01.0',
-    'JSESSIONID-WYYY': 'eo6OA1ctSOhigp6wgcp12D9%2B2llEji7N%2FWF1DmN2c%2BiQDZwws%2FmUnRH49uqSKQm8v2C9Nt0eWj7VCScMz%2BSDB72GiFUVZsdaVNnfIFof0U%2FXhZ5Bg4Zm%5CEh5%2FDnHKT%2FqDZSls243mwtHSdTNAliy%5CrNn6PoN7el9W3KhslIehDg9MUmO%3A1677593117192',
+    'WNMCID': 'dqynhy.1680829463077.01.0',
+    'WM_TID': 'YxDMEHuuqf9AEFUBVFaFahSqWDiMFV5d',
+    'FPTOKEN': 'TEpkUNAlvL799rywt8p/wL+wbG3UFmPCbFrC15If76oI0WjsW/HPO/WO2QcAqOFiDgcjB4iT/tuNBG357ZCoDLUwUl3uE8KOA3MZAalIMQFAdWYrepeiOVcAqP2yiBjJoMsxLTI2+yBYhKPs5i2EPtR4ZZMAj3QJXPs0HblxaovvgTYrHmilWXM9DE7drNF3dYmOtZ8FT6mTBagVSK0vriYxAFwpwXJdVOj6H5hYiBfTSE5T06twUOv4X45kTfV21cNSSMY4/OLO+21y8E9xgFRSleQkFbO+AVj3fNxwht38wtgsYq/rblOd+4JjeP7u7PljS/Euiw66M6j1nxdoc1s3P5LlD17UyoUrLUwdqdZc2ce0JKYC2hTIPIPptfSn3cUIaGSopnHqJj/h/A/Zyw==|MruS1jHKnpk5EZtETC+sysjuswKYAkknLHPIxTGkGzI=|10|2233f5d8e7da6c6ae8b6e9d9c39fdd03',
+    'JSESSIONID-WYYY': '%2Fhyyfb7gwZjJVISqZPHJkcdQ0R7FFJ1kG%2Bztl%5CKYS5%2Fquh%5CCalpmX%2Bkv7kInXgHOynRjGKxql57GKMY1lOQPqt6sUa6cH747DoeWUhYvHHOZ7ijmqqg5T9xOkfPe15Y1DrHxnNTst5EPrTFRmVOZc9IvoWURlFsjZXFlUtufemhlP3i3%3A1683041701991',
     '_iuqxldmzr_': '33',
-    'WM_NI': 'UKORbMl4049KH2O%2FC2OZ1%2FAPwOTaB9xg2WoljVlP6ruMggC1W4%2BpImGAFO%2F%2FI7R5JHJahClt%2BE95rS6LjjpsaRI6EDVidUqxzmVZOKPomzIQMDxuBx%2BXpUTPnmrBFGJLTEY%3D',
-    'WM_NIKE': '9ca17ae2e6ffcda170e2e6eeaadb48b2888694cf2582ef8ab6c15f968e8fb0d46b8e908ab4ae4a96edb797e92af0fea7c3b92a979581b3e147b0ec00d5bc418cee81d0c23a8d86ffd0c45cb5919c82c133bcb9feb8ee68aa8aa4b1b57ab793a988c23b95ece591cc67859f8ab0ae258be99ed6c9448abfa8a4ef80e99bf7d6f159f694a68bf379a38e87d7c85096b7acb6f474bab39abbce4194b1a39bcc5b8f9cb896c174e988c0b6d75caeb3bea3c54d8a949ca6dc37e2a3',
-    'playerid': '92343383',
+    'WM_NI': 'retX5IaF0w5CuNoJ0uyy72yG%2FsYSEKYdC2p6pkpMAH7Q4VMYGuUSyPEk25YdmRftP8ILyD4y2sHimkOycQRYkjqqlpVqB7YYQ33OwAkdyQopGLMm6KkF6%2BaaURjzjG6LdDQ%3D',
+    'WM_NIKE': '9ca17ae2e6ffcda170e2e6eeb3d63a9c99e5a4c64f8d9e8bb6c14f879e9f87c46bb2bfffccb663aab1be86d12af0fea7c3b92ab2bbbcb9b843a39a0098b849b792a1abb37fbba7f9d3fc4dedad97b4c8419bbc8dd3f33eb4b5c0aec444f58ffcb7d05385ab9bb4fc5c87b5fed6dc70af8eaa95d3348b94b7a5b773f2ad888af0618da7adb2d153a9b6bf92ef7c96af8ed8dc5393edfdb0f64ba9bcb7b2ee7c8594838eb541a99afacce46b978ea69be94df694aca5d437e2a3',
 }
 
 headers = {
     'authority': 'music.163.com',
     'accept': '*/*',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'content-length': '440',
     'content-type': 'application/x-www-form-urlencoded',
+    # 'cookie': '__bid_n=187385d55219a699254207; P_INFO=zawaludo@126.com|1680361713|0|mail126|00&99|null&null&null#gud&440100#10#0#0|&0|mail126|zawaludo@126.com; _ntes_nnid=e320573ab9dcf82fee6ce0bd1fde46ec,1680829462419; _ntes_nuid=e320573ab9dcf82fee6ce0bd1fde46ec; NMTID=00OUohNvG-YwixOHkjphD1aA8XEaIMAAAGHWT43ng; WEVNSM=1.0.0; WNMCID=dqynhy.1680829463077.01.0; WM_TID=YxDMEHuuqf9AEFUBVFaFahSqWDiMFV5d; FPTOKEN=TEpkUNAlvL799rywt8p/wL+wbG3UFmPCbFrC15If76oI0WjsW/HPO/WO2QcAqOFiDgcjB4iT/tuNBG357ZCoDLUwUl3uE8KOA3MZAalIMQFAdWYrepeiOVcAqP2yiBjJoMsxLTI2+yBYhKPs5i2EPtR4ZZMAj3QJXPs0HblxaovvgTYrHmilWXM9DE7drNF3dYmOtZ8FT6mTBagVSK0vriYxAFwpwXJdVOj6H5hYiBfTSE5T06twUOv4X45kTfV21cNSSMY4/OLO+21y8E9xgFRSleQkFbO+AVj3fNxwht38wtgsYq/rblOd+4JjeP7u7PljS/Euiw66M6j1nxdoc1s3P5LlD17UyoUrLUwdqdZc2ce0JKYC2hTIPIPptfSn3cUIaGSopnHqJj/h/A/Zyw==|MruS1jHKnpk5EZtETC+sysjuswKYAkknLHPIxTGkGzI=|10|2233f5d8e7da6c6ae8b6e9d9c39fdd03; JSESSIONID-WYYY=%2Fhyyfb7gwZjJVISqZPHJkcdQ0R7FFJ1kG%2Bztl%5CKYS5%2Fquh%5CCalpmX%2Bkv7kInXgHOynRjGKxql57GKMY1lOQPqt6sUa6cH747DoeWUhYvHHOZ7ijmqqg5T9xOkfPe15Y1DrHxnNTst5EPrTFRmVOZc9IvoWURlFsjZXFlUtufemhlP3i3%3A1683041701991; _iuqxldmzr_=33; WM_NI=retX5IaF0w5CuNoJ0uyy72yG%2FsYSEKYdC2p6pkpMAH7Q4VMYGuUSyPEk25YdmRftP8ILyD4y2sHimkOycQRYkjqqlpVqB7YYQ33OwAkdyQopGLMm6KkF6%2BaaURjzjG6LdDQ%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eeb3d63a9c99e5a4c64f8d9e8bb6c14f879e9f87c46bb2bfffccb663aab1be86d12af0fea7c3b92ab2bbbcb9b843a39a0098b849b792a1abb37fbba7f9d3fc4dedad97b4c8419bbc8dd3f33eb4b5c0aec444f58ffcb7d05385ab9bb4fc5c87b5fed6dc70af8eaa95d3348b94b7a5b773f2ad888af0618da7adb2d153a9b6bf92ef7c96af8ed8dc5393edfdb0f64ba9bcb7b2ee7c8594838eb541a99afacce46b978ea69be94df694aca5d437e2a3',
     'origin': 'https://music.163.com',
-    'referer': 'https://music.163.com/',
-    'sec-ch-ua': '"Not_A Brand";v="99", "Microsoft Edge";v="109", "Chromium";v="109"',
+    'referer': 'https://music.163.com/search/',
+    'sec-ch-ua': '"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.69',
-    'x-forwarded-for': '8.8.4.4',
-    'x-music-loc-site': '100_https://music.163.com/',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.39',
 }
 
 params = {
@@ -52,7 +45,7 @@ def search_music(song):
         # 'params': 'qcmGOmNTiR7q0qRyMLTaTupGxxFx7xRO4SVXpDk9E3fGmw5kxD9rP1mlPXsGPPTM2RQngXRKqcJknuhAa22FK96fVX2/7hSneAH6vBt1AwPrKQsaZUVb1DldBEioO1WnHmGQy5IsYyziFx20onvUHf71/hyeX31VobQE4dplNzm+o/mERYyKq/H9xXoNwEcXoRJ4rZA05qiXzQH2KMc0zzwapbgG3Tg6skv9O44kxW1iGm4Av0HqD5yGjkm4ULPGKD9Wa/+jeR0/03BqHzorsoweTC+3paB8bixKPdt0Cxw=',
         # 'encSecKey': 'd2d7cac9076735c7b649baaf52d8fd84c6f559dfd7cae887a8d300fe7ba02fd660e1db9b38add3a2713d88a4bab460bcff2ddab18c711c5d539b20c3be3cba458d25586b78ad02e33f4c3bb803853fde8fa56837dbcabf94094c723ec45a2536c871662c517ef50fa567d43547562f93e890bd01e6282d20dd30177692adeb71',
         'params': jscode['encText'],
-        'encSecKey': jscode['encSecKey']
+        'encSecKey':jscode['encSecKey']
     }
     try:
         response = requests.post(
@@ -62,68 +55,18 @@ def search_music(song):
             headers=headers,
             data=data,
         ).json()
-
-    except Exception as e:
-        print('Error', e, sep='\n')
-    else:
         # print(response['result']['songs'])
         ss = response['result']['songs']
         id_list = []
-        for i, s in enumerate(ss):
-            # 创作人有多个，需要使用字符串拼接列表元素，展示出来  例如 / Name: Infinity (PRETTY YOUNG Remix) / Artist -- Jaymes Young/PRETTY YOUNG
-            artists = ' / '.join([x['name'] for x in s['ar']])
-            print(i, '/ Name: '+s['name'], f'/ Artist -- {artists}')
-            id_list.append(dict(num=i, id=s['id'], name=s['name'], arname=s['ar'][0]['name']))
-        # print(id_list)
-        s_num = int(input('请选择上述哪一首歌曲进行下载(输入编号)'))
-        download_music(id_list[s_num]['id'], name=id_list[s_num]['name'], artist=id_list[s_num]['arname'])
+        for s in ss:
+            # print(s['id'],s['name'],s['ar'][0]['name'])
+            id_list.append(dict(id=s['id'], name=s['name'], arname=s['ar'][0]['name']))
 
-
-def download_music(s_id, **kwargs):
-    global params, cookies, headers
-    jscode = execjs.compile(open('param_encSeckey.js', 'r', encoding='utf-8').read()).call('get_info', s_id)
-    data = {
-        'params': jscode['encText'],
-        'encSecKey': jscode['encSecKey']
-    }
-    try:
-        response = requests.post(
-            'https://music.163.com/weapi/song/enhance/player/url/v1',
-            params=params,
-            cookies=cookies,
-            headers=headers,
-            data=data
-        ).json()
-        # print(response)
+        print(id_list)
     except Exception as e:
-        print('Get music_url Failed', e, sep='\n')
-    else:
-        m_url = response['data'][0]['url']
-        print('url:', m_url)
-        name, singer = kwargs['name'], kwargs['artist']
-        # print(name, singer)
-        try:
-            song_resp = requests.get(m_url)
-            form = m_url.split('.')[-1]
-            print('response_status--', song_resp.status_code, sep='\n')
-        except Exception as e:
-            print("Can't Download '{0}', Please try other way to download music".format(name), e, sep='\n')
-
-        else:
-            file_path = f'E:/Python/Spiders/JS_Reverse/NetEaseMusicDownload/Audio/{name}'
-            # 文件去重
-            if os.path.exists(f'{file_path}.{form}'):
-                print(f"{name + singer} already exists, skipping downlAsoad.")
-                return
-            try:
-                with open(f'{file_path}.{form}', mode='wb') as f:
-                    f.write(song_resp.content)
-                print(f'Song:{name}, Artist:{singer}, Downloaded!!!', sep='\t')
-            except Exception:
-                tb = traceback.format_exc()
-                print(tb)
+        print('Error', e, sep='\n')
 
 
 if __name__ == '__main__':
-    s_name = input('输入要搜索的歌曲名: ')
+    s_name = input('歌曲名:')
     search_music(s_name)
